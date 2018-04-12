4428baab681b55634ec03d5a81cb067e148306b5
Adam Pałaszyński <35331661+appalaszynski@users.noreply.github.com>
2018-04-12 15:16:59 +0200

    Put comments explaining the use of functions

--- length.js
+++ length.js
@@ -4,13 +4,22 @@
   * Licensed under MIT (https://github.com/appalaszynski/length.js/blob/master/LICENSE)
   */
 ;(function(global) {
+
+  // Main length function (availavlable by global.length) which is
+  // allowing us to create an object by calling 'length()' istead of 'new Length()'
   var length = function(value, unit) {
+    // Check if passed arguments are valid. If not - throw an error.
     validate(value, unit);
     return new Length(value, unit)
   }
 
+  // Current length.js version.
+  var version = "0.0.1";
+
+  // Currently supported units.
   var supportedUnits = ['cm', 'm'];
 
+  // Function used during new Length object creation. Check 'length' function.
   function validate(value, unit) {
     if (!value || !unit) {
       throw Error('You have to pass value and unit type!')
@@ -21,6 +30,7 @@
     }
   }
  
+  // Functions which are going to be available in every Length object prototype.
   function toMeter() {
     switch (this.unit) {
       case 'cm': {
@@ -43,15 +53,20 @@
     }
   }
 
+  // Initialize Length object prototype
   var proto = Length.prototype = {};
+  // Insert functions into Length object prototype
+  proto.version = version;
   proto.toMeter = toMeter;
   proto.toCentimeter = toCentimeter;
 
+  // The actual Length object is created here
   function Length(value, unit) {
     this.value = value || 1;
     this.unit = unit || 'cm';
   }
 
+  // Expose 'length' and 'L$' identifiers
   global.length = global.L$ = length;
   if(typeof exports === 'object' && typeof module !== 'undefined'){
     module.exports = length
